11f01e9aa1206f428735435e67802dfe3cd6ce04
Majid Lotfinia <majidlotfinia12@gmail.com>
2021-04-30 17:57:34 +0430

    remove config

--- api.ts
+++ api.ts
@@ -1,13 +1,7 @@
 import 'react-native-url-polyfill/auto';
-import {PetApi, UserApi, Configuration} from './generated';
-import axios from 'axios';
+import {PetApi, UserApi} from './generated';
 
-const config = new Configuration();
-const axiosInstance = axios.create({
-  headers: {Authorization: 'YOUR_TOKEN'},
-});
-
-const petApi = new PetApi(config, 'example.com', axiosInstance);
-const userApi = new UserApi(config, 'example.com', axiosInstance);
+const petApi = new PetApi();
+const userApi = new UserApi();
 
 export {petApi, userApi};
